d90445ae252a22ed53d9e7fd7d24fedc619ce3fe
beurmuz <fallinta2@gmail.com>
2022-10-06 00:57:46 +0900

    스타일 적용

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import React from "react";
+import postStyles from "../../styles/Post.module.css";
 import { getAllPostIds, getPostData, getSortedPostsData } from "../../lib/post";
 
 const Post = ({
@@ -13,7 +14,7 @@ const Post = ({
   };
 }) => {
   return (
-    <div>
+    <div className={postStyles.container}>
       <Head>
         <title>{postData.title}</title>
       </Head>
